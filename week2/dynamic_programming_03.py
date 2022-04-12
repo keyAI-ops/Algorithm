9d7692f79cab53c1a2b64d819297f15e4960032e
Chang_K <ckckckck1207@naver.com>
2022-04-12 21:11:26 +0900

    algorithm

--- week2/dynamic_programming_03.py
+++ week2/dynamic_programming_03.py
@@ -5,5 +5,17 @@ list_money = []
 
 
 for i in range(n):
-    a = int(input())
-    list_money.append(a)
+    list_money.append(int(input))
+
+d = [10001]*(m+1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(list_money[i], m+1):
+        if d[j-list_money[i]] != 10001:
+            d[j] = min(d[j], d[j-list_money[i]]+1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
