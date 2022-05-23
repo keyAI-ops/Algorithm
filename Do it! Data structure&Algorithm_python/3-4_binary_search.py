b165febe4fa4e73b7f09c38716b78668b689d4d5
Chang_K <ckckckck1207@naver.com>
2022-05-23 17:35:07 +0900

    binary_search&hash

---
+++ Do it! Data structure&Algorithm_python/3-4_binary_search.py
@@ -0,0 +1,21 @@
+from typing import Any, Sequence
+
+def bin_search(a:Sequence, key:Any) -> int:
+    pl = 0
+    pr = len(a)-1
+    
+    while True:
+        pc = (pl+pr)//2
+        
+        if a[pc] == key:
+            return pc
+        elif a[pc] < key:
+            pl = pc+1
+        else:
+            a[pc] > key
+            pr = pc-1
+        if pl > pr:
+            break
+        return -1
+    
+                
