89defe9160894d851e217d2ba3c0e267ca86e05f
KeyAI-ops <ckckckck1207@naver.com>
2022-08-11 00:05:22 +0900

    모험가 길드

--- Greedy&Implementation/greedy_03.py
+++ Greedy&Implementation/greedy_03.py
@@ -1,17 +1,18 @@
 # <문제> 모험가 길드
 
-n = int(input('인원수를 입력하세요 : '))
+def solution(n, data):
+    data.sort()
+    people_count = 0
+    group_count = 0
 
-data = list(map(int, input('공포도를 입력하세요 : ').split()))
-data.sort()
+    for i in data:
+        people_count += 1
+        if people_count >= i:
+            group_count += 1
+            people_count = 0
+    return group_count
 
-count = 0
-result = 0
-
-for i in data:
-    count += 1
-    if count >= i:
-        result += 1
-        count = 0
-        
-print(result)
+''' 문제해결
+    - 공포도가 가장 낮은 순으로 정렬하여 하나씩 빼오면서 확인
+    - 인원수가 공포도보다 크거나 같다면 그룹 하나 결성
+'''
