faa9ab6c7f76168e768fe8f5349103f3d5ba1070
KeyAI-ops <ckckckck1207@naver.com>
2022-08-11 01:01:11 +0900

    시각

--- Greedy&Implementation/implementation_02.py
+++ Greedy&Implementation/implementation_02.py
@@ -1,11 +1,11 @@
 # <문제> 시각
 
-n = int(input('N을 입력하세요 : '))
+def solution(n):
+    count = 0
+    for i in range(n+1):
+        for j in range(60):
+            for k in range(60):
+                if '3' in str(i) + str(j) + str(k):
+                    count += 1
+    return count
 
-count = 0
-for i in range(n+1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                count += 1
-print(count)
