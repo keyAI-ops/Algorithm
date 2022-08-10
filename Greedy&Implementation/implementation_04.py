2237dc6ad788145f926dbdcb726b77f811b2955c
KeyAI-ops <ckckckck1207@naver.com>
2022-08-11 01:29:51 +0900

    문자열 재정렬

--- Greedy&Implementation/implementation_04.py
+++ Greedy&Implementation/implementation_04.py
@@ -1,5 +1,25 @@
 # <문제> 문자열 재정렬
 
+from unittest import result
+
+
+def solution(pro_str):
+    alphabet_str = ""
+    num_str = ""
+    
+    for i in pro_str:
+        if ord(i) >= 65 and ord(i) <= 90:
+            alphabet_str += i
+        else:
+            num_str += i
+    alphabet_str = "".join(sorted(alphabet_str))
+    num_str  = "".join(sorted(num_str))
+    result = alphabet_str + num_str
+    print(result)
+
+solution("K1KA5CB7")
+        
+'''
 str_num = list(input('문자열을 입력하세요 : '))
 
 str_num.sort()
@@ -17,3 +37,4 @@ result2 = ''.join(result2)
 result3 = result2+str(result)
 
 print(result3)
+'''
