3df598b4ebc855baddaebdb44b99939ad5b9fc31
KeyAI-ops <ckckckck1207@naver.com>
2022-08-13 22:29:59 +0900

    정렬된 배열에서 특정 수의 개수 구하기_test

--- Binary_Search/binary_search_02_test.py
+++ Binary_Search/binary_search_02_test.py
@@ -5,11 +5,9 @@ from bisect import bisect_left, bisect_right
 n, x = map(int, input('N과 x를 입력하세요 : ').split())
 array = list(map(int, input().split()))
 
+answer = bisect_right(array, x) - bisect_left(array, x)
 
-def count_by_range(a, x):
-    right_index = bisect_right(a, x)
-    left_index = bisect_left(a, x)
-    print(right_index - left_index)
-
-
-count_by_range(array, x)
+if answer > 0:
+    print(answer)
+else:
+    print(-1)
