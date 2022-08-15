32012ad15bd698da04f7bb2afdb2d2b2ebd66a6b
KeyAI-ops <ckckckck1207@naver.com>
2022-08-15 22:33:19 +0900

    개미 전사

--- Dynamic_programming/dynamic_programming_01.py
+++ Dynamic_programming/dynamic_programming_01.py
@@ -1,12 +1,12 @@
-n = int(input('식량창고의 개수를 입력하시오 : '))
-k = list(map(int, input('식량의 개수를 입력하시오 : ').split()))
+# <문제> 개미 전사
 
-d = [0]*100
+def dynamic_ant(n, k):
+    d = [0]*100
 
-d[0] = k[0]
-d[1] = max(k[0], k[1])
+    d[0] = k[0]
+    d[1] = max(k[0], k[1])
 
-for i in range(2, n):
-    d[i] = max(d[i-1], d[i-2] + k[i])
-
-print(d[n-1])
+    for i in range(2, n):
+        d[i] = max(d[i-1], d[i-2] + k[i])   
+        
+    return d
